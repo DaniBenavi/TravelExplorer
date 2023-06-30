c0b0ee4ff1ea80bd7184ee2007f9b482589e4a75
DaniBenavi <danibenavi1907@gmail.com>
2023-06-29 23:11:59 -1200

    Feat(Tests)implementacion de UnitTesting en Customer

---
+++ tests/UnitTests/Application.Customers.UnitTests/Create/CreateCustomerCommandHandlerUnitTests.cs
@@ -0,0 +1,40 @@
+using Application.Customers.Create;
+using Domain.Customers;
+using Domain.Primitives;
+using Domain.DomainErrors;
+
+namespace Application.Customers.UnitTests.Create;
+
+public class CreateCustomerCommandHandlerUnitTests
+{
+    private readonly Mock<ICustomerRepository> _mockCustomerRepository;
+    private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+
+    private readonly CreateCustomerCommandHandler _handler;
+    public CreateCustomerCommandHandlerUnitTests()
+    {
+        _mockCustomerRepository = new Mock<ICustomerRepository>();
+        _mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        _handler = new CreateCustomerCommandHandler(_mockCustomerRepository.Object, _mockUnitOfWork.Object);
+    }
+    [Fact]
+    public async void HandleCreateCustomer_WhenPhoneNumberHasBadFormat_ShouldReturnValidationError()
+    {
+        CreateCustomerCommand command = new CreateCustomerCommand(
+            "Daniel",
+            "Benavides",
+            "danibenavi@gmail.com",
+            "6546546523",
+            "", "", "", "", "", ""
+            );
+
+
+        var result = await _handler.Handle(command, default);
+
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorOr.ErrorType.Validation);
+        result.FirstError.Code.Should().Be(Errors.Customer.PhoneNumberWhitBadFormat.Code);
+        result.FirstError.Description.Should().Be(Errors.Customer.PhoneNumberWhitBadFormat.Description);
+    }
+}
